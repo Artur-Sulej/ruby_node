089bc8502890f449e633df96b29ec67437b52bfb
Artur Sulej <artur@shedul.com>
2024-03-02 11:20:15 +0100

    Simplified simple example

--- simple_port_example/reverse.ex
+++ simple_port_example/reverse.ex
@@ -1,25 +1,17 @@
-defmodule ReversePort do
-  def start do
-    Port.open({:spawn, "ruby reverse.rb"}, [:binary])
+port = Port.open({:spawn, "ruby reverse.rb"}, [:binary])
+
+Port.command(port, "No palindromes here!\n")
+
+result =
+  receive do
+    {^port, {:data, data}} -> data
+  after
+    2000 -> {:error, "Timeout! No data received from Ruby!"}
   end
 
-  def send_data(port, data) do
-    Port.command(port, "#{data}\n")
-  end
-
-  def receive_data(port) do
-    receive do
-      {^port, {:data, data}} -> data
-    after
-      2000 -> {:error, :timeout}
-    end
-  end
-end
-
-{:ok, port} = ReversePort.start()
-ReversePort.send_data(port, "No palindromes here!")
-result = ReversePort.receive_data(port)
 IO.puts("Result: #{inspect(result)}")
+
+Port.close(port)
 
 # Run:
 # elixir reverse.ex
--- simple_port_example/reverse.rb
+++ simple_port_example/reverse.rb
@@ -1,5 +1,5 @@
 $stdout.sync = true
 
-while input = gets
-  puts "Greetings from Ruby: #{input.strip.reverse}"
-end
+input = gets
+puts "Greetings from Ruby: #{input.strip.reverse}"
+sleep
